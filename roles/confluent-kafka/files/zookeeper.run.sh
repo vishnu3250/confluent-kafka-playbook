5648079cc889da89934870c493d491f89ac3f8ce
vis <vishnunimmalapudi@gmail.com>
2022-01-10 20:52:18 +0000

    added user path instead of root path for kafka installationand also removed defaults/main.yml

--- roles/confluent-kafka/files/zookeeper.run.sh
+++ roles/confluent-kafka/files/zookeeper.run.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 APP_NAME="Zookeeper"
-export CMAK_HOME=/opt/app/confluent/cmak-3.0.0.5
-export JAVA_HOME=/opt/app/java/jdk-11.0.1
-export CONFLUENT_HOME=/opt/app/confluent/confluent-6.2.0
+export CMAK_HOME=/home/vishnunimmalapudi/confluent.temp/confluent/cmak-3.0.0.5
+export JAVA_HOME=/home/vishnunimmalapudi/confluent.temp/java/jdk-11.0.1
+export CONFLUENT_HOME=/home/vishnunimmalapudi/confluent.temp/confluent/confluent-6.2.0
 export ZK_HOSTS="fraboxpd0071.de.db.com:2181,fraboxpd0072.de.db.com:2181"
 export PATH=$PATH:$JAVA_HOME/bin:$CONFLUENT_HOME/bin:$CMAK_HOME/bin
 echo "$APP_NAME : Launching"
-zookeeper-server-start /opt/app/service/zookeeper/zookeeper.properties
+zookeeper-server-start /home/vishnunimmalapudi/confluent.temp/service/zookeeper/zookeeper.properties
 
