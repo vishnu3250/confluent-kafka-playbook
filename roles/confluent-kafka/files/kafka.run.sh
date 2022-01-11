18eb75591e02b7762d3bde8ee3db09eba3948e5f
vis <vishnunimmalapudi@gmail.com>
2022-01-11 12:20:11 +0000

    sdcfd

---
+++ roles/confluent-kafka/files/kafka.run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+APP_NAME="Kafka Broker $1"
+export CMAK_HOME=/home/vishnunimmalapudi/confluent.temp/confluent/cmak-3.0.0.5
+export JAVA_HOME=/home/vishnunimmalapudi/confluent.temp/java/jdk-11.0.1
+export CONFLUENT_HOME=/home/vishnunimmalapudi/confluent.temp/confluent/confluent-6.2.0
+export ZK_HOSTS="fraboxpd0071.de.db.com:2181,fraboxpd0072.de.db.com:2181"
+export PATH=$PATH:$JAVA_HOME/bin:$CONFLUENT_HOME/bin:$CMAK_HOME/bin
+echo "$APP_NAME : Launching"
+kafka-server-start /home/vishnunimmalapudi/confluent.temp/service/kafka/server-$1.properties
+
